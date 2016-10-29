7b6a2d13aff9cf186290d6cf714cb5c4313abbcf
新井 元気 <garai@crooz.local>
2016-10-29 18:12:26 +0900

    initial commit

---
+++ Starwars_Kotolinz/app/src/main/java/com/example/garai/starwars_kotolin/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.garai.starwars_kotolin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
